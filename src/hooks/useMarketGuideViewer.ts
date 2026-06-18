dc6c904faa42b5f1c921d935b0be70e485ff34db
rickchoi <sbner182@gmail.com>
2026-06-18 16:20:02 +0900

    feat: dev market guide viewer.

--- src/hooks/useMarketGuideViewer.ts
+++ src/hooks/useMarketGuideViewer.ts
@@ -2,7 +2,33 @@ import { useEffect, useRef, useState } from 'react';
 import * as d3 from 'd3';
 import axios from 'axios';
 
-type StoreShape = {
+// type StoreShape = {
+//   id: string;
+//   x: number;
+//   y: number;
+//   width: number;
+//   height: number;
+//   fill: string;
+//   class: string;
+//   storeName?: string;
+// };
+
+// type MarketGuideData = {
+//   sectionName: string;
+//   stores: StoreShape[];
+// };
+
+// type StoreNameEntry = {
+//   id: string;
+//   store_name: string;
+// }
+
+// const INITIAL_GUIDE_DATA: MarketGuideData = {
+//   sectionName: "",
+//   stores: [],
+// };
+
+type StoreDataShape = {
   id: string;
   x: number;
   y: number;
@@ -10,96 +36,149 @@ type StoreShape = {
   height: number;
   fill: string;
   class: string;
-  storeName?: string;
+  storeName: string;
+  stroke: string;
+  strokeWidth:string;
 };
 
-type MarketGuideData = {
-  sectionName: string;
-  stores: StoreShape[];
+type UseStoreDataShape = Partial<StoreDataShape>;
+
+type MarketGuideMeta = {
+  section_name: string;
+  stores: UseStoreDataShape[];
 };
 
-type StoreNameEntry = {
-  id: string;
-  store_name: string;
-}
-
-const INITIAL_GUIDE_DATA: MarketGuideData = {
-  sectionName: "",
-  stores: [],
-};
+// const INITIAL_GUIDE_META: MarketGuideMeta = {
+//   sectionName: "",
+//   stores: []
+// }
 
 const useMarketGuideViewer = (dataUrl: string) => {
   const svgRef = useRef<SVGSVGElement | null>(null);
-  const [guideData, setGuideData] = useState<MarketGuideData>(INITIAL_GUIDE_DATA);
+  // const [guideData, setGuideData] = useState<MarketGuideData>(INITIAL_GUIDE_DATA);
+  const [marketGuide, setMarketGuide] = useState<MarketGuideMeta[]>([]);
 
   useEffect(() => {
     const fetchGuideData = async () => {
-      const [sectionRes, storeNameRes] = await Promise.all([
-        axios.get(dataUrl),
-        axios.get("/local_data/test_store_name.json"),
-      ]);
+      const [ section_1, section_2 ] = await Promise.all([
+        axios.get("/local_data/market_section_3-1.json"),
+        axios.get("/local_data/market_section_3-2.json")
+      ])
 
-      const sectionName = sectionRes.data.section.section_name as string;
-      const storesData = sectionRes.data.section.stores as StoreShape[];
-      const storeNames = storeNameRes.data as StoreNameEntry[];
+      const sectionMeta_1 = section_1.data.section;
+      const sectionMeta_2 = section_2.data.section;
 
-      const storeNameMap = new Map(storeNames.map((entry) => [entry.id, entry.store_name]));
-
-      setGuideData({
-        sectionName,
-        stores: storesData.map((store) => ({
-          ...store,
-          storeName: storeNameMap.get(store.id) ?? "",
-        })),
-      })
+      setMarketGuide([sectionMeta_2]);
     }
 
     fetchGuideData();
-  }, [dataUrl])
+  }, [])
 
   useEffect(() => {
-    if (!guideData) return;
+    if (!marketGuide) return;
     const svg = d3.select(svgRef.current);
     let section: d3.Selection<SVGGElement, unknown, null, undefined> | null = null;
-    // let stores: d3.Selection<d3.BaseType | SVGRectElement, StoreShape, SVGGElement, unknown> | null = null;
-    let storeGruops: d3.Selection<d3.BaseType | SVGGElement, StoreShape, SVGGElement, unknown> | null = null;
+    let stores: d3.Selection<d3.BaseType | SVGRectElement, UseStoreDataShape, SVGGElement, unknown> | null = null;
+    let storeGruops: d3.Selection<d3.BaseType | SVGGElement, UseStoreDataShape, SVGGElement, unknown> | null = null;
 
     svg.selectAll('g').remove();
     svg.selectAll('rect').remove(); 
-    svg.selectAll('text').remove(); 
 
-    section = svg.append('g')
-      .attr('id', guideData.sectionName);
+    console.log({ marketGuide });
 
-    storeGruops = section.selectAll('g')
-      .data(guideData.stores, (store) => (store as StoreShape).id)
-      .join('g')
-      .attr('id', (store) => `group-${store.id}`)
+    marketGuide.map((el) => {
+      console.log(el.section_name);
 
-    storeGruops.append('rect')
-      .attr('id', (store) => store.id)
-      .attr('class', (store) => store.class)
-      .attr('x', (store) => store.x)
-      .attr('y', (store) => store.y)
-      .attr('width', (store) => store.width)
-      .attr('height', (store) => store.height)
-      .attr('fill', (store) => store.fill);
+      section = svg.append('g')
+      .attr('id', el.section_name);
 
-    storeGruops.append('text')
-      .attr('x', (store) => store.x + store.width / 2)
-      .attr('y', (store) => store.y + store.height / 2)
-      .attr('text-anchor', 'middle')
-      .attr('dominant-baseline', 'middle')
-      .attr('font-size', 6)
-      .attr('writing-mode', (store) =>
-        store.height > store.width 
-        ? "tb" : ''
-      )
-      .text((store) => store.storeName ?? '');
+      storeGruops = section.selectAll('g')
+        .data(el.stores, (store) => (store as StoreDataShape).id)
+        .join('g')
+        .attr('id', (store) => `group-${store.id}`)
 
-  }, [guideData])
+      storeGruops.append('rect')
+        .attr('id', (store) => (store as StoreDataShape).id)
+        .attr('class', (store) => (store as StoreDataShape).class)
+        .attr('x', (store) => (store as StoreDataShape).x)
+        .attr('y', (store) => (store as StoreDataShape).y)
+        .attr('width', (store) => (store as StoreDataShape).width)
+        .attr('height', (store) => (store as StoreDataShape).height)
+        .attr('fill', (store) => (store as StoreDataShape).fill)
+        .attr('stroke', (store) => (store as StoreDataShape).stroke)  
+    })
 
-  return { svgRef, guideData };
+  }, [marketGuide])
+
+  // useEffect(() => {
+  //   const fetchGuideData = async () => {
+  //     const [sectionRes, storeNameRes] = await Promise.all([
+  //       axios.get(dataUrl),
+  //       axios.get("/local_data/test_store_name.json"),
+  //     ]);
+
+  //     const sectionName = sectionRes.data.section.section_name as string;
+  //     const storesData = sectionRes.data.section.stores as StoreShape[];
+  //     const storeNames = storeNameRes.data as StoreNameEntry[];
+
+  //     const storeNameMap = new Map(storeNames.map((entry) => [entry.id, entry.store_name]));
+
+  //     setGuideData({
+  //       sectionName,
+  //       stores: storesData.map((store) => ({
+  //         ...store,
+  //         storeName: storeNameMap.get(store.id) ?? "",
+  //       })),
+  //     })
+  //   }
+
+  //   fetchGuideData();
+  // }, [dataUrl])
+
+  // useEffect(() => {
+  //   if (!guideData) return;
+  //   const svg = d3.select(svgRef.current);
+  //   let section: d3.Selection<SVGGElement, unknown, null, undefined> | null = null;
+  //   // let stores: d3.Selection<d3.BaseType | SVGRectElement, StoreShape, SVGGElement, unknown> | null = null;
+  //   let storeGruops: d3.Selection<d3.BaseType | SVGGElement, StoreShape, SVGGElement, unknown> | null = null;
+
+  //   svg.selectAll('g').remove();
+  //   svg.selectAll('rect').remove(); 
+  //   svg.selectAll('text').remove(); 
+
+  //   section = svg.append('g')
+  //     .attr('id', guideData.sectionName);
+
+  //   storeGruops = section.selectAll('g')
+  //     .data(guideData.stores, (store) => (store as StoreShape).id)
+  //     .join('g')
+  //     .attr('id', (store) => `group-${store.id}`)
+
+  //   storeGruops.append('rect')
+  //     .attr('id', (store) => store.id)
+  //     .attr('class', (store) => store.class)
+  //     .attr('x', (store) => store.x)
+  //     .attr('y', (store) => store.y)
+  //     .attr('width', (store) => store.width)
+  //     .attr('height', (store) => store.height)
+  //     .attr('fill', (store) => store.fill);
+
+  //   storeGruops.append('text')
+  //     .attr('x', (store) => store.x + store.width / 2)
+  //     .attr('y', (store) => store.y + store.height / 2)
+  //     .attr('text-anchor', 'middle')
+  //     .attr('dominant-baseline', 'middle')
+  //     .attr('font-size', 6)
+  //     .attr('writing-mode', (store) =>
+  //       store.height > store.width 
+  //       ? "tb" : ''
+  //     )
+  //     .text((store) => store.storeName ?? '');
+
+  // }, [guideData])
+
+  // return { svgRef, guideData };
+    return { svgRef, marketGuide };
 };
 
 export default useMarketGuideViewer;
